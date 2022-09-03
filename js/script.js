2d5dd92cddd53b647a7fd68eb2d6e08296fffb49
abdullah <abdullah15-12557@diu.edu.bd>
2022-09-03 08:39:06 +0600

    before modal

--- js/script.js
+++ js/script.js
@@ -10,8 +10,19 @@ const newsURL = async(id) => {
         `https://openapi.programming-hero.com/api/news/category/${id}`
     );
     const data = await res.json();
-    console.log(data);
-}
+    console.log(data.data);
+    cardData(data.data);
+};
+
+//News details URL
+const newsDetailsURL = async(id) => {
+    const res = await fetch(
+        `https://openapi.programming-hero.com/api/news/${id}`
+    );
+    const data = await res.json();
+    console.log(data.data);
+    detailsModal(data.data);
+};
 
 // Category section
 const createCategory = (info) => {
@@ -27,5 +38,69 @@ const createCategory = (info) => {
     });
 };
 
+//Card data
+const cardData = (data) => {
+    const cardElement = document.getElementById("cardElement");
+    const numberOfCate = document.getElementById("numberOfCate");
+    data.forEach((data) => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div onclick="newsDetailsURL('${data._id}')" for="my-modal" class="card grid-cols-12 card-side bg-base-100 shadow-xl my-3">
+                <figure><img class="rounded-lg grid-cols-3" src="${data.thumbnail_url}" alt="Movie"></figure>
+                <div class="card-body grid-cols-9">
+                    <h2 class="text-3xl">${data.title}</h2>
+                    <p>${data.details}</p>
+                    <div class="card-actions flex justify-between align-center">
+                        
+                        <div class="flex align-center m-auto">
+                            <div class="avatar">
+                                <div class="w-14 rounded-full">
+                                    <img src="${data.author.img}" />
+                                </div>
+                            </div>
+
+                            <div class="pl-3 flex flex-col m-auto">
+                                <h1 class="text-xl">${data.author.name}</h1>
+                                <p>${data.author.published_date}</p>
+                            </div>
+
+                        </div>
+
+                        
+                        <div class="m-auto">
+                            <h1 class="text-xl"><i class="fa-solid fa-eye"></i> ${data.total_view}</h1>
+                        </div>
+
+                       
+                        <div class="m-auto">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                        </div>
+
+                        
+                        <div class="m-auto">
+                            <i class="fa-solid fa-right-long text-xl"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        cardElement.appendChild(div);
+    });
+    // const items = document.createElement("div");
+    // items.innerHTML = `<h1 class="text-2xl p-3 m-3">${cardElement.legnth} items found for category Entertainment</h1>`;
+    // numberOfCate.appendChild(items);
+};
+
+//Modal
+const detailsModal = (data) => {
+    const modalItems = document.getElementById("modalItems");
+    const div = document.createElement("div");
+    div.innerHTML = ``;
+    modalItems.appendChild(div);
+};
 
 fetchUrl();
